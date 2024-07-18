00959aad1a5eee432a518df6002720c5b9c6cfbb
Kapil Bhatia <111693509+Kapil-Sunil-Bhatia@users.noreply.github.com>
2024-07-18 12:47:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let dayBox = document.getElementById("day-box");
 let hrBox = document.getElementById("hr-box");
 let minBox = document.getElementById("min-box");
 let secBox = document.getElementById("sec-box");
-let endDate = new Date(2023, 0, 1, 00, 00);
+let endDate = new Date(2025, 0, 1, 00, 00);
 let endTime = endDate.getTime();
 
 function countdown() {
